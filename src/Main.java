bd62c93ca809839321464b43987d4f79fb9bec0d
Empoliam <liamjwright@btinternet.com>
2016-11-12 11:15:36 +0000

    Can now enable and disable crosshair

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,20 @@ import org.apache.commons.math3.complex.Complex;
 public class Main {
 
 	static int ITERATIONS = 255;
-	static double IMAGE_X = 4000;
-	static double IMAGE_Y = 4000;
-	static double ZOOM = 750.0;
-	static double CENTRE_X = -0.747;
-	static double CENTRE_Y = 0.0996;
+	static double IMAGE_X = 2000;
+	static double IMAGE_Y = 2000;
+	static double ZOOM = 1.0;
+	static double CENTRE_X = -0.75;
+	static double CENTRE_Y = 0.1;
+	static boolean CROSSHAIR = true;
 
 	static BufferedImage output = new BufferedImage((int)IMAGE_X,(int)IMAGE_Y,BufferedImage.TYPE_INT_RGB);
 
 	public static void main(String args[])
 	{
-		
+
+		long START = System.nanoTime();
+
 		for(int x = 0; x < (int)IMAGE_X; x ++){
 
 			for (int y = 0; y < (int)IMAGE_Y; y++){
@@ -31,10 +34,12 @@ public class Main {
 				int escape = check(new Complex(real,img));
 
 				if(escape == ITERATIONS+1) output.setRGB(x,y,0x000000);
-				else output.setRGB(x, y, new Color(escape%255,escape%255,0).getRGB());
+				else output.setRGB(x, y, new Color(escape%256,escape%256,0).getRGB());
 
-				if(x == IMAGE_X/2) output.setRGB(x, y, Color.WHITE.getRGB());
-				if(y == IMAGE_Y/2) output.setRGB(x, y, Color.WHITE.getRGB());
+				if(CROSSHAIR){
+					if(x == IMAGE_X/2) output.setRGB(x, y, Color.WHITE.getRGB());
+					if(y == IMAGE_Y/2) output.setRGB(x, y, Color.WHITE.getRGB());
+				}
 
 			}
 
@@ -42,7 +47,11 @@ public class Main {
 
 		File f = new File("mandelbrot.png");
 		try { ImageIO.write(output, "PNG", f); }
-		catch(IOException e){System.out.println("Failed to print");};
+		catch(IOException e){System.out.println("Failed to print"); };
+
+		long END = System.nanoTime();
+
+		System.out.println((END - START) / 1000000000);
 
 	}
 
@@ -54,7 +63,7 @@ public class Main {
 
 		for(int k = 1; k <= ITERATIONS; k++){
 
-			z = z.multiply(z);
+			z = z.multiply(z);			
 			z = z.add(c);
 
 			if((z.getReal()*z.getReal()) + (z.getImaginary()*z.getImaginary()) > 2){
