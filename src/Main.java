33a7abc3ce99fb9520274453f51dd1b6640afef3
Empoliam <liamjwright@btinternet.com>
2016-11-12 09:47:18 +0000

    Zoom fixed
    Swapped scale and transform in equation

--- src/Main.java
+++ src/Main.java
@@ -10,34 +10,28 @@ import org.apache.commons.math3.complex.Complex;
 public class Main {
 
 	static int ITERATIONS = 255;
-	static double IMAGE_X = 2000;
-	static double IMAGE_Y = 2000;
-	static double ZOOM = 1.0;
-	static double CENTRE_X = -0.75;
-	static double CENTRE_Y = 0.1;
+	static double IMAGE_X = 4000;
+	static double IMAGE_Y = 4000;
+	static double ZOOM = 750.0;
+	static double CENTRE_X = -0.747;
+	static double CENTRE_Y = 0.0996;
 
 	static BufferedImage output = new BufferedImage((int)IMAGE_X,(int)IMAGE_Y,BufferedImage.TYPE_INT_RGB);
 
 	public static void main(String args[])
 	{
-
+		
 		for(int x = 0; x < (int)IMAGE_X; x ++){
 
 			for (int y = 0; y < (int)IMAGE_Y; y++){
 
-				double real = ((x - ( IMAGE_X/2)) * (3 / IMAGE_X) + CENTRE_X) / ZOOM;
-				double img =  ((y - (IMAGE_Y/2)) * (3 / IMAGE_Y) - CENTRE_Y) / ZOOM;
+				double real = ((x - ( IMAGE_X/2)) * (3 / IMAGE_X) / ZOOM) + CENTRE_X;
+				double img =  ((y - (IMAGE_Y/2)) * (3 / IMAGE_Y) / ZOOM) - CENTRE_Y;
 
 				int escape = check(new Complex(real,img));
 
 				if(escape == ITERATIONS+1) output.setRGB(x,y,0x000000);
-				else{
-
-					if(escape*3 < 256) output.setRGB(x, y, new Color(escape%255,0,0).getRGB());
-					if(escape*3 < 512) output.setRGB(x, y, new Color(0,escape%255,0).getRGB());
-					else output.setRGB(x, y, new Color(0,0,escape%255).getRGB());
-
-				}
+				else output.setRGB(x, y, new Color(escape%255,escape%255,0).getRGB());
 
 				if(x == IMAGE_X/2) output.setRGB(x, y, Color.WHITE.getRGB());
 				if(y == IMAGE_Y/2) output.setRGB(x, y, Color.WHITE.getRGB());
@@ -49,7 +43,6 @@ public class Main {
 		File f = new File("mandelbrot.png");
 		try { ImageIO.write(output, "PNG", f); }
 		catch(IOException e){System.out.println("Failed to print");};
-
 
 	}
 
